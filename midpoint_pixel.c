df27455a8b5a4344a67effdafca1a2cde44fc069
Prajwal <prazwolkhatiwada@gmail.com>
2024-01-28 10:42:44 +0545

    white

--- midpoint_pixel.c
+++ midpoint_pixel.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
 #include <conio.h>
+#include <dos.h>
 #include <graphics.h>
 
 void drawCircle(int x1, int y1, int r);
 
 int main()
-
 {
-
     int gd = DETECT, gm;
-
     int x, y, r;
 
     printf("Enter the Midpoint and Radius: ");
-
     scanf("%d%d%d", &x, &y, &r);
 
     initgraph(&gd, &gm, "C://TURBOC3//BGI");
 
+    // Set color to WHITE
+    setcolor(WHITE);
+
     drawCircle(x, y, r);
 
     getch();
-
     closegraph();
-
     return 0;
 }
 
 void drawCircle(int x1, int y1, int r)
-
 {
-
     int x = 0, y = r;
-
     int p = 1 - r;
 
     void plotPoints(int, int, int, int);
@@ -40,44 +35,30 @@ void drawCircle(int x1, int y1, int r)
     plotPoints(x1, y1, x, y);
 
     while (x < y)
-
     {
-
         x++;
-
         if (p < 0)
-
             p += 2 * x + 1;
-
         else
-
         {
-
             y--;
-
             p += 2 * (x - y) + 1;
         }
-
         plotPoints(x1, y1, x, y);
     }
 }
 
 void plotPoints(int xctr, int yctr, int x, int y)
-
 {
-    putpixel(xctr + x, yctr + y, 1);
-
-    putpixel(xctr - x, yctr + y, 1);
-
-    putpixel(xctr + x, yctr - y, 1);
-
-    putpixel(xctr - x, yctr - y, 1);
-
-    putpixel(xctr + y, yctr + x, 1);
-
-    putpixel(xctr - y, yctr + x, 1);
-
-    putpixel(xctr + y, yctr - x, 1);
-
-    putpixel(xctr - y, yctr - x, 1);
+    // Set color to WHITE
+    setcolor(WHITE);
+    delay(100);
+    putpixel(xctr + x, yctr + y, WHITE);
+    putpixel(xctr - x, yctr + y, WHITE);
+    putpixel(xctr + x, yctr - y, WHITE);
+    putpixel(xctr - x, yctr - y, WHITE);
+    putpixel(xctr + y, yctr + x, WHITE);
+    putpixel(xctr - y, yctr + x, WHITE);
+    putpixel(xctr + y, yctr - x, WHITE);
+    putpixel(xctr - y, yctr - x, WHITE);
 }
